505c1ab0a797ac8900f99a8efc44dcc2f399b7d2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-01-02 12:09:40 +0530

    Finish dependency graph serialization project

--- io/java-serialization/10-project-2/deserialization/src/b/DeserializationDemo.java
+++ io/java-serialization/10-project-2/deserialization/src/b/DeserializationDemo.java
@@ -1,8 +1,22 @@
 package b;
 
+import shared.Customer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class DeserializationDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        File file = new File("customer.db");
+        FileInputStream fis = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(fis);
 
+        Customer customer = (Customer) ois.readObject();
+
+        ois.close();
+
+        System.out.println(customer);
     }
 }
--- io/java-serialization/10-project-2/shared/src/shared/Customer.java
+++ io/java-serialization/10-project-2/shared/src/shared/Customer.java
@@ -1,12 +1,13 @@
 package shared;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
     private String id;
-    private Name name;
+    private transient Name name;
     private String address;
 
     public Customer(String id, Name name, String address) {
@@ -25,11 +26,22 @@ public class Customer implements Serializable {
     }
 
     private void writeObject(ObjectOutputStream out) throws IOException {
-        out.writeObject(id);
+        out.defaultWriteObject();
+        //out.writeObject(id);
         out.writeObject(name.getFirstName());
         out.writeObject(name.getMiddleName());
         out.writeObject(name.getLastName());
-        out.writeObject(address);
+        //out.writeObject(address);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        //this.id = (String) in.readObject();
+        String firstName = (String) in.readObject();
+        String middleName = (String) in.readObject();
+        String lastName = (String) in.readObject();
+        this.name = new Name(firstName, middleName, lastName);
+        //this.address = (String) in.readObject();
     }
 
 }
